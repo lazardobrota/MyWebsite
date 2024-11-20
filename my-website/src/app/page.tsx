0a996c1b656ab8785097a241120a9b977a61dd83
Lazar Dobrota <lazardobrota1323@gmail.com>
2024-11-21 00:58:24 +0100

    add inner shadow to whole page

--- my-website/src/app/page.tsx
+++ my-website/src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
 
   return (
     <>
-    <div className="flex flex-col gap-32"> {/* px-36 za pc*/}
+    <div className="flex flex-col gap-32 shadow-inner-sm"> {/* px-36 za pc*/}
       <Main></Main>
       <MyWork></MyWork>
       <AboutMe></AboutMe>
--- my-website/tailwind.config.ts
+++ my-website/tailwind.config.ts
@@ -17,7 +17,9 @@ export default {
         '4xl': [
             '0 35px 35px rgba(0, 0, 0, 0.25)',
             '0 45px 65px rgba(0, 0, 0, 0.15)'
-        ]
+        ],
+        'inner-sm': ' inset 0px 2px 20px 4px rgba(0, 0, 0, 0.25)',
+        'inner-m': ' inset 0px 2px 20px 11px rgba(0, 0, 0, 0.25)',
       }
     },
   },
