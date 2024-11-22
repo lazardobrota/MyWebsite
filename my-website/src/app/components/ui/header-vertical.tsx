e839252d536ca9c8924de59a72f5c928af273021
Lazar Dobrota <lazardobrota1323@gmail.com>
2024-11-22 23:51:51 +0100

    make animation more clean

--- my-website/src/app/components/ui/header-vertical.tsx
+++ my-website/src/app/components/ui/header-vertical.tsx
@@ -2,10 +2,10 @@
 export default function HeaderVertical() {
   return (
     <div className="flex flex-col gap-7 items-center justify-star fixed top-96 right-14 z-10">
-      <a className="size-3 border-2 border-darkpurple-100 bg-darkpurple-100 rotate-45 transition-transform hover:bg-transparent hover:scale-175 hover:rotate-0 ease-out" href="#main" />
-      <a className="size-3 border-2 border-darkpurple-100 bg-darkpurple-100 rotate-45 transition-transform hover:bg-transparent hover:scale-175 hover:rotate-0 ease-out" href="#my-work"/>
-      <a className="size-3 border-2 border-darkpurple-100 bg-darkpurple-100 rotate-45 transition-transform hover:bg-transparent hover:scale-175 hover:rotate-0 ease-out" href="#about-me" />
-      <a className="size-3 border-2 border-darkpurple-100 bg-darkpurple-100 rotate-45 transition-transform hover:bg-transparent hover:scale-175 hover:rotate-0 ease-out" href="#contact-me" />
+      <a className="size-3 border-2 border-darkpurple-100 bg-darkpurple-100 rotate-45 transition-all hover:bg-transparent hover:scale-175 hover:rotate-0 ease-out" href="#main" />
+      <a className="size-3 border-2 border-darkpurple-100 bg-darkpurple-100 rotate-45 transition-all hover:bg-transparent hover:scale-175 hover:rotate-0 ease-out" href="#my-work"/>
+      <a className="size-3 border-2 border-darkpurple-100 bg-darkpurple-100 rotate-45 transition-all hover:bg-transparent hover:scale-175 hover:rotate-0 ease-out" href="#about-me" />
+      <a className="size-3 border-2 border-darkpurple-100 bg-darkpurple-100 rotate-45 transition-all hover:bg-transparent hover:scale-175 hover:rotate-0 ease-out" href="#contact-me" />
     </div>
   )
 }
--- my-website/src/app/pages/footer.tsx
+++ my-website/src/app/pages/footer.tsx
@@ -4,7 +4,7 @@ import linkedin_logo from "../images/icons/linkedin-logo-white.svg";
 
 export default function Footer() {
   return (
-    <div className="flex flex-row-reverse gap-4 relative pt-20 pb-10 px-5">
+    <div className="flex flex-row-reverse gap-4 relative pt-20 pb-10 px-5 lg:px-36">
       <BackgroundBottom preserveAspectRatio="none" className="left-0 top-0 size-full absolute -z-10" />
       <a href="https://github.com/lazardobrota">
         <img src={github_logo.src} alt="Github" />
