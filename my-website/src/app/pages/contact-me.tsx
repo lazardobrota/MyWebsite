c3a2dd429a7a0ae4febce18eee18cc5972b61ec0
Lazar Dobrota <lazardobrota1323@gmail.com>
2024-11-22 14:31:37 +0100

    add correct stretch to input

--- my-website/src/app/pages/contact-me.tsx
+++ my-website/src/app/pages/contact-me.tsx
@@ -3,17 +3,19 @@ import Input from "../components/ui/input";
 
 export default function ContactMe() {
   return (
-    <div id="contact-me" className="flex flex-col gap-20 items-center px-5">
+    <div id="contact-me" className="flex flex-col gap-20 items-center px-8 lg:px-36">
       <p className="font-bold text-4xl">Contact Me</p>
-      <div className="flex flex-col gap-16 items-center">
+      <div className="flex flex-col gap-16 w-full max-lg:max-w-3xl">
         <div className="flex flex-col gap-6 lg:gap-12">
-          <div className="flex flex-wrap gap-6">
+          <div className="flex flex-col lg:flex-row gap-6">
             <Input name="Name"  placeholder="Your Name"></Input>
             <Input name="Email" placeholder="Your Email"></Input>
           </div>
           <Input name="Message" placeholder="Your Message"></Input>
         </div>
-        <Button name="Submit"></Button>
+        <div className="flex justify-center">
+          <Button name="Submit"></Button>
+        </div>
       </div>
     </div>
   )
