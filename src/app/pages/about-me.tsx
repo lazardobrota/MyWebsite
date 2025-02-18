d2635db64e74d971728b702c9f733b8ee610c03b
lazardobrota <lazardobrota1323@gmail.com>
2025-02-18 21:29:56 +0100

    refactor: make text more informative

--- src/app/pages/about-me.tsx
+++ src/app/pages/about-me.tsx
@@ -6,7 +6,7 @@ import BackgroundMiddle from "../components/Icons/bg-middle";
 import { RefInfo } from "../types/reference";
 import { LegacyRef } from "react";
 
-export default function AboutMe({ref}: RefInfo) {
+export default function AboutMe({ ref }: RefInfo) {
 
   return (
     <div ref={ref as LegacyRef<HTMLDivElement>} id="about-me" className="flex flex-col gap-20 lg:gap-28 items-center py-32 lg:py-44 relative p-5 lg:px-36">
@@ -18,15 +18,22 @@ export default function AboutMe({ref}: RefInfo) {
         <div className="flex flex-col gap-8 max-lg:items-center">
           <p className="font-bold text-3xl lg:text-h2 text-white">Engineering</p>
           <div className="lg:w-1/2">
-            <p className="text-xl lg:text-p1 text-white">I am building application with Java in the backend following architectures. I have used Microservices, Spring, ...</p>
+            <p className="text-xl text-white">
+              My software engineering journey began four years ago, and I am now in my final year of Computer Science.
+              Throughout this time, I’ve worked on both solo and team projects, ranging from a distributed system with a custom SSTable implementation to fun side projects like a Flappy Bird clone.
+              I enjoy tackling complex technical challenges and continuously expanding my knowledge in system architecture, databases, and efficient software design.
+            </p>
           </div>
         </div>
       </div>
       <div className="flex lg:flex-row-reverse">
         <div className="flex flex-col gap-8 max-lg:items-center lg:w-1/2">
-          <p className="font-bold text-3xl lg:text-h2 text-white lg:items-start">GameDev</p>
+          <p className="font-bold text-3xl lg:text-h2 text-white lg:items-start">Exploring</p>
           <div>
-            <p className="text-xl lg:text-p1 text-white">I love creating amazing games using Unity Engine. I have created many really simple games and now I am working a actual game for publishing.</p>
+            <p className="text-xl text-white">
+              I constantly challenge myself to learn new things, whether it's designing and building this website or exploring new concepts. 
+              I enjoy researching various topics and creating my own versions, and sometimes I take it a step further by developing something entirely original — like the game I'm currently working on for publishing.
+            </p>
           </div>
         </div>
       </div>
--- src/app/pages/main.tsx
+++ src/app/pages/main.tsx
@@ -4,20 +4,20 @@ import my_logo from "../images/icons/my-logo.svg";
 import me_picture from "../images/me.png";
 import { RefInfo } from "../types/reference";
 
-export default function Main({ref}: RefInfo) {
-  
+export default function Main({ ref }: RefInfo) {
+
   return (
     <div ref={ref as LegacyRef<HTMLDivElement>} id="main" className="flex flex-col xl:flex-row-reverse gap-8 xl:gap-36 place-items-center relative pt-16 pb-32 xl:pt-44 xl:pb-96 px-5 xl:px-36">
-      <BackgroundTop preserveAspectRatio="none" className="size-full absolute top-0 left-0 -z-10"/>
-      {/* <img className="size-20 xl:absolute xl:top-7 xl:left-7" src={my_logo.src}/> */}
-      <img className="min-w-48 min-h-48 w-2/6 max-w-80 basis-4/6" src={me_picture.src}/>
+      <BackgroundTop preserveAspectRatio="none" className="size-full absolute top-0 left-0 -z-10" />
+      <img className="min-w-48 min-h-48 w-2/6 max-w-80 basis-4/6" src={me_picture.src} />
       <div className="flex flex-col gap-8 max-xl:place-items-center">
         <p className="font-bold text-4xl text-white xl:text-h1">
           Hellooooo;
         </p>
         <p className="max-xl:text-center text-white xl:text-xl">
-        My name is Lazar Dobrota, and I am a Software Engineer, building a wide range of backend projects and designing custom frontend interfaces for some of them. I focus on clean architecture, scalability and even though I like Java I don’t drink coffee.
-          {/* My name is Lazar Dobrota, and I am a Software Engineer, building a wide range of backend projects and designing custom frontend interfaces for some of them. I'm also passionate about gaming and game development. */}
+          I'm Lazar Dobrota, a Software Engineer passionate about building scalable backend systems and designing custom frontend interfaces when needed. 
+          I focus on clean architecture and performance optimization. 
+          And while I enjoy coding in Java, I don't drink coffee.
         </p>
       </div>
     </div>
